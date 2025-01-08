935fbb2997c8648de007b57f952e50574af5f245
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2025-01-07 18:25:49 -0800

    Создал структуру проекта и основные файлы

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://stellarburgers.nomoreparties.site/api/"
+
+ENDPOINTS = {
+    "register_user": "auth/register",
+    "login_user": "auth/login",
+    "update_user": "auth/user",
+    "create_order": "orders",
+    "get_user_orders": "orders"
+}
